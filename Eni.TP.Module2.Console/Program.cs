fc2b42f2b8652b2d753e27d991e9eebde5ade960
nfauveau2020 <nfauveau2020@1SE20-2WQJVN2>
2022-05-16 16:18:45 +0200

    Implémentation des valeurs

--- Eni.TP.Module2.Console/Program.cs
+++ Eni.TP.Module2.Console/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace Eni.TP.Module2.Console
 {
+    using System;
+    using Eni.TP.Module2.Lib;
     class Program
     {
         static void Main(string[] args)
         {
+            var formes = new List<Forme>();
+            formes.Add(new Cercle { Rayon = 3 });
+            formes.Add(new Rectangle { Largeur = 3, Longueur = 4 });
+            formes.Add(new Carre { Longueur = 3 });
+            formes.Add(new Triangle { A = 4, B = 5, C = 6 });
+            foreach (var forme in formes)
+            {
+                Console.WriteLine(forme);
+            }
+            Console.ReadKey();
         }
     }
 }
