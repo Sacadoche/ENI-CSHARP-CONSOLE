c04e49bbd11469f1b6589e7e9e57dbc8b8f11e2a
nfauveau2020 <nfauveau2020@1SE20-2WQJVN2>
2022-05-16 14:47:06 +0200

    Ajout de la classe fonctionnelle fruit

---
+++ Eni.Demo.Module2.Lib/Eni.Demo.Module2.Lib/Fruit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eni.Demo.Module2.Lib
+{
+    public abstract class Fruit
+    {
+        public DateTime DatePeremption { get; set; }
+
+        public Fruit(DateTime consomabilite)
+        {
+            this.DatePeremption = DateTime.Now.AddDays(consomabilite.Day);
+        }
+    }
+}
