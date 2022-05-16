fe8c4e90e31d10efe83efca6b2005b1f2b76722f
nfauveau2020 <nfauveau2020@1SE20-2WQJVN2>
2022-05-16 14:23:46 +0200

    Ajoutez des fichiers projet.

---
+++ Eni.Demo.Module2.Console/Eni.Demo.Module2.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eni.Demo.Module2.Console
+{
+    using Eni.Demo.Module2.Lib;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personne = new Personne(0);
+            for (int i = 0;i<50; i++)
+            {
+                System.Console.WriteLine(personne.Age++);
+            }
+
+            System.Console.Read();
+        }
+    }
+
+}
+
---
+++ Eni.Demo.Module2.Lib/Eni.Demo.Module2.Lib/Personne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eni.Demo.Module2.Lib
+{
+    public class Personne
+    {
+        private int age;
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public string Prenom { get; set; }
+
+        public Personne(int age)
+        {
+            this.age = age;
+        }
+    }
+}
