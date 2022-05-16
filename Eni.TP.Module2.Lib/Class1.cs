41839e879144f39c75d63323a370f6e8b640aa2e
nfauveau2020 <nfauveau2020@1SE20-2WQJVN2>
2022-05-16 15:06:58 +0200

    init new TP project

---
+++ Eni.TP.Module2.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eni.TP.Module2.Lib
+{
+    public class Class1
+    {
+    }
+}
